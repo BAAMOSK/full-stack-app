d9ed6038f3ed70baa572ac8c04d8e2bd3d5fe88d
BAAMOSK <bunth667@gmail.com>
2017-07-28 14:20:51 -0400

    working on dev env

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const { mongoURI, PORT, cookieKey } = require('../config/keys');
+const { mongoURI, PORT, cookieKey } = require('./config/keys');
+
 require('./models/User');
 require('./services/passport');
 
