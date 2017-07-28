443802483f919df6c77404e44774f427fae85422
BAAMOSK <bunth667@gmail.com>
2017-07-28 00:40:27 -0400

    removing morgan

--- server/index.js
+++ server/index.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const morgan = require('morgan');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-
-app.use(morgan('combined'));
 
 app.listen(PORT, () => {
   console.log(`Server is running on localhost:${PORT}`);
