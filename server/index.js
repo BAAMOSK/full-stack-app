a305f97c044e9bf871572d452f371559398981a3
BAAMOSK <bunth667@gmail.com>
2017-07-28 14:35:48 -0400

    fixed port variable

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const { mongoURI, PORT, cookieKey } = require('./config/keys');
-
+const { mongoURI, cookieKey } = require('./config/keys');
+const PORT = process.env.PORT || 3000;
 require('./models/User');
 require('./services/passport');
 
