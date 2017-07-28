47840129cc195b74b8a4d71d166a7fa720bdbee2
BAAMOSK <bunth667@gmail.com>
2017-07-28 00:06:42 -0400

    server running

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const logger = require('morgan');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(logger('combined'));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on localhost:${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Batman');
+})
